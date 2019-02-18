e3c96308898bbf87fe577e20fac9a9ba4ef3949a
Irisedmonson <irisedmonson@gmail.com>
2019-02-18 10:33:51 -0500

    Added Project

---
+++ 3401_Week5_Edmonson/Assets/Scenes/Scripts/PlayerDetector.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDetector : MonoBehaviour
+{
+    public Transform destination;
+        void FixedUpdate()
+        {
+            Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+        if (Physics.Raycast(transform.position, fwd, 5))
+        {
+            Debug.Log(GameObject.Find (name));
+        }
+
+        else
+        {
+            Debug.Log("NothingAhead");
+        }
+         }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+}
